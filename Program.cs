f8534b4a9e12c29ab909ae5ea56ffa2fb63dfe07
BaoHG <baohuynh291@gmail.com>
2024-04-09 20:06:38 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter size of array: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Enter element {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        int max = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+        Console.WriteLine($"The largest property value in the list is {max}");
+    }
+}
